8f88e6b9f04afe1659be6498313105ae46f4fca8
zhangsen <717849952@qq.com>
2022-12-20 19:28:12 +0800

    1220编写

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
-})
+}),
+{
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': 'src/assets',
+        'common': 'src/common',
+        'components': 'src/components',
+        'network': 'src/network',
+        'views': 'src/views',
+      }
+    }
+  }
+}
